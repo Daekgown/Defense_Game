15c3f133574168e9a70d6787354cb2d3b017625c
Daekgown <dlaeornjs012@naver.com>
2022-06-06 17:15:01 +0900

    (사운드 편)효과음 관리

--- Defense/Assets/Script/MyType.cs
+++ Defense/Assets/Script/MyType.cs
@@ -2,4 +2,5 @@
 public class MyType
 {
     public enum Unit { Sword, Range, Guard, Wizard, Bullet };
+    public enum Sound { Victory, Defeat, Buy, Upgrade, Sword, Range, RangeHit, Guard, Magic};
 }
